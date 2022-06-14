f80896a6a4b9cf6c125a7432d8e206044cb1e57a
lucasvegini <lucas.vegini@farmaciasapp.com.br>
2022-06-14 20:21:44 -0300

    login page design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Text } from '@develop-fapp/ui-kit-fapp';
+import { Button, Container, Text, Input } from '@develop-fapp/ui-kit-fapp';
 import { useRouter } from 'next/router';
 
 export default function Home() {
@@ -7,29 +7,38 @@ export default function Home() {
   return (
     <Container
       container="fluid"
-      style={{ height: '100vh' }}
+      style={{
+        height: '100vh', backgroundImage: `url(background.png)`, backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center'
+
+      }}
       justifyContent="center"
       alignItems="center"
       flexDirection="column"
+      backgroundColor='#17222F'
+
     >
-      <Text style={{ marginBottom: '16px' }}>
-        Pagina de login em construção
-      </Text>
-      <Text style={{ marginBottom: '16px' }}>
-        Para acessar os níveis da federação ou do representante de clube utilize
-        os botões abaixo
-      </Text>
-      <Container justifyContent="center">
-        <Button
-          variant="contained"
-          onClick={() => router.push('/federacao')}
-          style={{ marginRight: '16px' }}
-        >
-          Fededação
-        </Button>
-        <Button variant="contained" onClick={() => router.push('/clube')}>
-          Representante de clube
-        </Button>
+      <Container style={{ borderRadius: '8px', padding: '16px', width: '300px' }} container='sm' backgroundColor='white' justifyContent="center"
+        alignItems="center"
+        flexDirection="column" >
+        <Text weight='bold' style={{ marginBottom: '32px' }} >
+          Faça seu login
+        </Text>
+
+        <Input
+          placeholder="E-mail"
+        />
+
+        <div style={{ marginBottom: '16px' }} />
+
+        <Input
+          placeholder="Senha"
+        />
+
+        <div style={{ marginBottom: '16px' }} />
+
+        <Button variant='contained' >Continuar</Button>
+
       </Container>
     </Container>
   );
